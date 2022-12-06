67ca5b040653daff52cf69b3c760b3dfe2ce83d5
Ethan Burkett <ethan@ethanburkett.net>
2022-12-06 00:28:50 -0600

    added buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ScriptProps } from "next/script";
 import React, { useEffect, useRef } from "react";
 import { BiChevronDown } from "react-icons/bi";
@@ -70,6 +71,40 @@ const Hero = (props: ScriptProps) => {
             One of the best roleplay servers that you can find on FiveM. active
             departments and professional staff.
           </p>
+          <br />
+        </div>
+        <div className="bottom-[25vh] z-50 absolute flex gap-6 flex-row justify-center items-center">
+          <Link href="fivem://connect/play.blaineccrp.com">
+            <p className="bg-hover relative orange flex flex-row  items-center text-center justify-evenly gap-2 cursor-pointer p-2 px-6 border-2 border-[var(--color)] uppercase font-light transition-all">
+              <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6 mr-2"
+                fill="#EEEEEE"
+              >
+                <title>FiveM</title>
+                <path d="M22.4 24h-5.225c-.117 0-.455-1.127-1.026-3.375-1.982-6.909-3.124-10.946-3.417-12.12l3.37-3.325h.099c.454 1.42 2.554 7.676 6.299 18.768ZM12.342 7.084h-.048a3.382 3.385 0 0 1-.098-.492v-.098a102.619 102.715 0 0 1 3.272-3.275c.13.196.196.356.196.491v.05a140.694 140.826 0 0 1-3.322 3.324ZM5.994 10.9h-.05c.67-2.12 1.076-3.209 1.223-3.275L14.492.343c.08 0 .258.524.533 1.562zm1.37-4.014h-.05C8.813 2.342 9.612.048 9.71 0h4.495v.05a664.971 664.971 0 0 1-6.841 6.839Zm-2.69 7.874h-.05c.166-.798.554-1.418 1.174-1.855a312.918 313.213 0 0 1 5.71-5.717h.05c-.117.672-.375 1.175-.781 1.52zM1.598 24l-.098-.05c1.399-4.172 2.148-6.322 2.248-6.45l6.74-6.694v.05C10.232 11.88 8.974 16.263 6.73 24Z" />
+              </svg>
+              Join Server
+            </p>
+          </Link>
+          <Link href="https://discord.gg/bccrp">
+            <p className="bg-hover relative blurple flex flex-row  items-center text-center justify-evenly cursor-pointer border-2 border-[var(--color)] p-2 px-6 uppercase font-light transition-all">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-discord w-6 h-6 mr-4"
+                viewBox="0 0 16 16"
+              >
+                {" "}
+                <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />{" "}
+              </svg>
+              Discord
+            </p>
+          </Link>
         </div>
       </div>
     </div>
